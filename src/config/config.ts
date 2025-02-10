55becd7e658bba384e32d90316a7b0a6890af93e
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-02-10 17:12:18 +0530

    Update config.ts

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,14 +1,18 @@
-import dotenv from 'dotenv';
-import path from 'path';
+import dotenv from "dotenv";
+import path from "path";
 
-const envFile = `.env.${process.env.NODE_ENV || 'development'}`;
+const envFile = `.env.${process.env.NODE_ENV || "development"}`;
 
 dotenv.config({ path: path.resolve(process.cwd(), envFile) });
 
 export const config = {
   PORT: process.env.PORT || 5000,
-  API_HEADER_KEY: process.env.API_HEADER_KEY || 'your-valid-api-key',
+  API_HEADER_KEY: process.env.API_HEADER_KEY || "your-valid-api-key",
   DATABASE: {
-    DB_NAME: process.env.DB_NAME || 'mongodb://localhost:27017',
+    DB_NAME: process.env.DB_NAME || "mongodb://localhost:27017",
   },
+  ACCESS_TOKEN_SECRET:
+    process.env.ACCESS_TOKEN_SECRET || "tripLoom_8001_development",
+  REFRESH_TOKEN_SECRET:
+    process.env.REFRESH_TOKEN_SECRET || "Triploom#8001#development",
 };
