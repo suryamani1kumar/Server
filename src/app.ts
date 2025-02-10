7c2caca1308f6b193169dd5f643b172580ad074a
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-02-10 15:19:57 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,11 @@
 import express, { Express } from 'express';
-import connectToDataBase from './DBConnection';
+// import connectToDataBase from './DBConnection';
 import configMiddlewares from './middlewares/config.middleware';
 import { Routes } from './routes/all.routes';
 
 const app: Express = express();
 
-connectToDataBase();
+// connectToDataBase();
 
 configMiddlewares.forEach((middlewares) => app.use(middlewares));
 
