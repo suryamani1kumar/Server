ac15a9239cb2c92dc674059a874a0949e65a2242
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-02-10 15:10:02 +0530

    implement cluster

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,21 @@
-import app from './app';
-import { config } from './config/config';
+import cluster from "cluster";
+import os from "os";
+import app from "./app";
+import { config } from "./config/config";
 
-app.listen(config.PORT, () => {
-  console.log(`[server]: Server is running at http://localhost:${config.PORT}`);
-});
+const numberOfCUP = os.cpus().length;
+if (cluster.isPrimary) {
+  for (let i = 0; i < numberOfCUP; i++) {
+    cluster.fork();
+  }
+  cluster.on("exit", (worker) => {
+    console.log(`Worker ${worker.process.pid} died. Restarting...`);
+    cluster.fork();
+  });
+} else {
+  app.listen(config.PORT, () => {
+    console.log(
+      `[Worker ${process.pid}]: Server is running at http://localhost:${config.PORT}`
+    );
+  });
+}
