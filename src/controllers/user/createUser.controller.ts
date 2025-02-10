4c02ca707ad01f7e793e1bab97798e121dc9dbe4
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-02-10 15:10:52 +0530

    Update createUser.controller.ts

--- src/controllers/user/createUser.controller.ts
+++ src/controllers/user/createUser.controller.ts
@@ -1,17 +1,30 @@
-import { Request, Response } from 'express';
-import saveJsonToFile from '../../services/saveJson';
-import usersData from '../../data/usersData.json';
+import { Request, Response } from "express";
+import saveJsonToFile from "../../services/saveJson";
+import usersData from "../../data/usersData.json";
+import { v4 as uuid } from "uuid";
+import { hashPassword } from "../../services/password";
 
-export const CreateUser = (req: Request, res: Response) => {
-  const findUser = usersData.find(
-    (user) =>
-      user.email.trim().toLowerCase() === req.body.email.trim().toLowerCase()
-  );
+export const CreateUser = async (req: Request, res: Response) => {
+  const RequestData = req.body;
+  const findUser =
+    usersData.length > 0 &&
+    usersData.find(
+      (user) =>
+        user.email.trim().toLowerCase() === req.body.email.trim().toLowerCase()
+    );
   if (findUser) {
-    res.send('User is allready exist');
+    res.send("User is allready exist");
     return;
   }
-  saveJsonToFile('usersData.json', req.body);
-
-  res.send('ok');
+  const hashedPassword = await hashPassword(RequestData.password);
+  const baseUsername = RequestData.email.split("@")[0].replace(/\s+/g, "");
+  const randomNum = Math.floor(1000 + Math.random() * 9000);
+  RequestData.password = hashedPassword;
+  RequestData.username = `${baseUsername}${randomNum}`;
+  RequestData.id = uuid();
+  RequestData.isActive = true;
+  RequestData.createdAt = new Date().toISOString();
+  RequestData.updatedAt = RequestData.createdAt;
+  saveJsonToFile("usersData.json", req.body);
+  res.send("ok");
 };
