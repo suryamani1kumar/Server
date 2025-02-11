4cafc2f0885b4c38248a54739b925b1f4fd60259
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-02-11 14:46:48 +0530

    Update saveJson.ts

--- src/services/saveJson.ts
+++ src/services/saveJson.ts
@@ -1,24 +1,67 @@
-import fs from 'fs';
-import path from 'path';
+import fs from "fs";
+import path from "path";
+import { Usertype } from "./type";
 
 const filePathName = (fileName: string) => {
-  return path.join('src', 'data', fileName);
+  return path.join("src", "data", fileName);
 };
 
-const saveJsonToFile = (fileName: string, data: any): void => {
+const saveJsonToFile = (fileName: string, data: Usertype): void => {
   try {
-    let existingData: any[] = [];
+    let existingData: Usertype[] = [];
     const existfile = fs.existsSync(filePathName(fileName));
     if (existfile) {
-      const fileContent = fs.readFileSync(filePathName(fileName), 'utf8');
+      const fileContent = fs.readFileSync(filePathName(fileName), "utf8");
       existingData = fileContent ? JSON.parse(fileContent) : [];
     }
     existingData.push(data);
     const insertData = JSON.stringify(existingData, null, 2);
-    fs.writeFileSync(filePathName(fileName), insertData, 'utf8');
+    fs.writeFileSync(filePathName(fileName), insertData, "utf8");
   } catch (errorMessage) {
-    console.error('Error writing to file:', errorMessage);
+    console.error("Error writing to file:", errorMessage);
   }
 };
 
+export const updateJsonFile = (
+  fileName: string,
+  id: string,
+  fieldName: string,
+  fieldvalue: string | boolean
+): void => {
+  fs.readFile(filePathName(fileName), "utf-8", (err, data) => {
+    if (err) {
+      console.error("Error reading users data:", err);
+      return;
+    }
+
+    try {
+      let findData = JSON.parse(data); // Parse JSON file
+
+      // Find the user index in the array
+      const userIndex = findData.findIndex(
+        (user: { id: string }) => user.id === id
+      );
+
+      if (userIndex !== -1) {
+        findData[userIndex][fieldName] = fieldvalue; // Update value
+
+        // Write the updated array back to the JSON file asynchronously
+        fs.writeFile(
+          filePathName(fileName),
+          JSON.stringify(findData, null, 2),
+          "utf-8",
+          (err) => {
+            if (err) {
+              console.error("Error updating Lastlogin:", err);
+            }
+          }
+        );
+      }
+    } catch (error) {
+      console.error("Error parsing JSON:", error);
+    }
+  });
+};
+
 export default saveJsonToFile;
+
