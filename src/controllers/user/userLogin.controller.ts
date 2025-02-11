395db90dd377f9d4ad00e6a7f83150dea602324c
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-02-11 14:50:46 +0530

    Update userLogin.controller.ts

--- src/controllers/user/userLogin.controller.ts
+++ src/controllers/user/userLogin.controller.ts
@@ -1,43 +1,63 @@
-import { Request, Response } from 'express';
-import usersData from '../../data/usersData.json';
-import { comparePassword } from '../../services/password';
+import { Request, Response } from "express";
+import usersData from "../../data/usersData.json";
+import { comparePassword } from "../../services/password";
 import {
   generateAccessToken,
   generateRefreshToken,
-} from '../../services/token';
+} from "../../services/token";
+import { Usertype } from "../../services/type";
+import { updateJsonFile } from "../../services/saveJson";
+
+const UsersData = usersData as Usertype[];
 
 export const userLogin = async (req: Request, res: Response) => {
   const { password, userIdOrmail } = req.body;
 
-  const findUser = usersData.find(
-    (user: { email: string; username: string }) => {
+  if (!userIdOrmail || !password) {
+    res
+      .status(400)
+      .json({ message: "Email/Username and Password are required" });
+    return;
+  }
+
+  const findUser = UsersData.find(
+    (user) =>
       user.email.trim().toLowerCase() === userIdOrmail.trim().toLowerCase() ||
-        user.username.trim().toLowerCase() ===
-          userIdOrmail.trim().toLowerCase();
-    }
+      user.username.trim().toLowerCase() === userIdOrmail.trim().toLowerCase()
   );
 
   if (!findUser) {
-    res.status(404).json({ message: 'user does not exist' });
+    res.status(404).json({ message: "user does not exist" });
     return;
   }
+
   const checkPassword = await comparePassword(password, findUser.password);
   if (!checkPassword) {
-    res.status(400).json({ message: 'Invalid credentials' });
+    res.status(400).json({ message: "Invalid credentials" });
     return;
   }
+
   const accessToken = generateAccessToken(req.body);
   const refreshToken = generateRefreshToken(req.body);
-  res.cookie('accessToken', accessToken, {
+
+  updateJsonFile(
+    "usersData.json",
+    findUser.id,
+    "Lastlogin",
+    new Date().toISOString()
+  );
+
+  res.cookie("a_token", accessToken, {
     httpOnly: true,
     secure: true,
-    sameSite: 'strict',
+    sameSite: "strict",
   });
 
-  res.cookie('refreshToken', refreshToken, {
+  res.cookie("r_token", refreshToken, {
     httpOnly: true,
     secure: true,
-    sameSite: 'strict',
+    sameSite: "strict",
   });
-  res.status(200).json({ message: 'Login successful!' });
+
+  res.status(200).json({ message: "Login successful!" });
 };
