0eae5a5d6bdabb966e7b1b066bd4babd6d2ac654
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-02-11 07:43:12 +0530

    merge and update

--- src/controllers/user/createUser.controller.ts
+++ src/controllers/user/createUser.controller.ts
@@ -1,23 +1,21 @@
-import { Request, Response } from "express";
-import saveJsonToFile from "../../services/saveJson";
-import usersData from "../../data/usersData.json";
-import { v4 as uuid } from "uuid";
-import { hashPassword } from "../../services/password";
+import { Request, Response } from 'express';
+import saveJsonToFile from '../../services/saveJson';
+import usersData from '../../data/usersData.json';
+import { v4 as uuid } from 'uuid';
+import { hashPassword } from '../../services/password';
 
 export const CreateUser = async (req: Request, res: Response) => {
   const RequestData = req.body;
-  const findUser =
-    usersData.length > 0 &&
-    usersData.find(
-      (user) =>
-        user.email.trim().toLowerCase() === req.body.email.trim().toLowerCase()
-    );
+  const findUser = usersData.find(
+    (user: { email: string }) =>
+      user.email.trim().toLowerCase() === req.body.email.trim().toLowerCase()
+  );
   if (findUser) {
-    res.send("User is allready exist");
+    res.send('User is allready exist');
     return;
   }
   const hashedPassword = await hashPassword(RequestData.password);
-  const baseUsername = RequestData.email.split("@")[0].replace(/\s+/g, "");
+  const baseUsername = RequestData.email.split('@')[0].replace(/\s+/g, '');
   const randomNum = Math.floor(1000 + Math.random() * 9000);
   RequestData.password = hashedPassword;
   RequestData.username = `${baseUsername}${randomNum}`;
@@ -25,6 +23,6 @@ export const CreateUser = async (req: Request, res: Response) => {
   RequestData.isActive = true;
   RequestData.createdAt = new Date().toISOString();
   RequestData.updatedAt = RequestData.createdAt;
-  saveJsonToFile("usersData.json", req.body);
-  res.send("ok");
+  saveJsonToFile('usersData.json', req.body);
+  res.send('ok');
 };
--- src/controllers/user/userLogin.controller.ts
+++ src/controllers/user/userLogin.controller.ts
@@ -1,39 +1,43 @@
-import { Request, Response } from "express";
-import usersData from "../../data/usersData.json";
-import { comparePassword } from "../../services/password";
+import { Request, Response } from 'express';
+import usersData from '../../data/usersData.json';
+import { comparePassword } from '../../services/password';
 import {
   generateAccessToken,
   generateRefreshToken,
-} from "../../services/token";
+} from '../../services/token';
 
 export const userLogin = async (req: Request, res: Response) => {
   const { password, userIdOrmail } = req.body;
+
   const findUser = usersData.find(
-    (user) =>
+    (user: { email: string; username: string }) => {
       user.email.trim().toLowerCase() === userIdOrmail.trim().toLowerCase() ||
-      user.username.trim().toLowerCase() === userIdOrmail.trim().toLowerCase()
+        user.username.trim().toLowerCase() ===
+          userIdOrmail.trim().toLowerCase();
+    }
   );
 
-  if (!findUser){  
-    res.status(404).json({ message: "user does not exist" });
-    return
-                }
+  if (!findUser) {
+    res.status(404).json({ message: 'user does not exist' });
+    return;
+  }
   const checkPassword = await comparePassword(password, findUser.password);
-  if (!checkPassword){
-     res.status(400).json({ message: "Invalid credentials" });return
+  if (!checkPassword) {
+    res.status(400).json({ message: 'Invalid credentials' });
+    return;
   }
   const accessToken = generateAccessToken(req.body);
   const refreshToken = generateRefreshToken(req.body);
-  res.cookie("accessToken", accessToken, {
+  res.cookie('accessToken', accessToken, {
     httpOnly: true,
     secure: true,
-    sameSite: "strict",
+    sameSite: 'strict',
   });
 
-  res.cookie("refreshToken", refreshToken, {
+  res.cookie('refreshToken', refreshToken, {
     httpOnly: true,
     secure: true,
-    sameSite: "strict",
+    sameSite: 'strict',
   });
-  res.status(200).json({ message: "Login successful!" });
+  res.status(200).json({ message: 'Login successful!' });
 };
