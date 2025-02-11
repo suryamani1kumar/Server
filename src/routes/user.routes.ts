ba03ace0d931190bfbc7a4e24cec720889f93b83
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-02-11 14:48:42 +0530

    Update user.routes.ts

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,10 +1,12 @@
-import { Router } from 'express';
-import { CreateUser } from '../controllers/user/createUser.controller';
-import { userLogin } from '../controllers/user/userLogin.controller';
+import { Router } from "express";
+import { CreateUser } from "../controllers/user/createUser.controller";
+import { userLogin } from "../controllers/user/userLogin.controller";
+import { logout } from "../controllers/user/userLogout.controller";
 
 const router = Router();
 
-router.post('/createUser', CreateUser);
-router.post('/login', userLogin);
+router.post("/createUser", CreateUser);
+router.post("/login", userLogin);
+router.post("/logout", logout);
 
 export default router;
