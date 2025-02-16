f9629f6f7a91ed9efce98cd37c059774c1fb39e1
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-02-16 22:40:54 +0800

    cors allows

--- src/middlewares/config.middleware.ts
+++ src/middlewares/config.middleware.ts
@@ -1,30 +1,17 @@
 import authenticateApiKey from './header.Auth';
 import cors from 'cors';
-import express, { NextFunction, RequestHandler } from 'express';
+import express, { RequestHandler } from 'express';
 import cookieParser from 'cookie-parser';
 
-// const setCookieMiddleware = (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
-//   // Set a cookie named "sessionId"
-//   res.cookie('sessionId', '123456', {
-//     httpOnly: true, // Makes the cookie inaccessible to client-side JavaScript
-//     secure: true, // Only send cookie over HTTPS in production
-//     sameSite: 'strict', // Protects against CSRF attacks
-//     maxAge: 24 * 60 * 60 * 1000, // 1 day in milliseconds
-//   });
-
-//   // Proceed to the next middleware or route handler
-//   next();
-// };
+const corsOptions = {
+  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  credentials: true,
+};
 
 const middlewares: RequestHandler[] = [
-  cors(),
+  cors(corsOptions),
   express.json(),
   cookieParser(),
-  // setCookieMiddleware,
   authenticateApiKey,
 ];
 
