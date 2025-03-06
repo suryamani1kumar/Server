9d8382cb5e1e039c214ea3d0d688a7c0172b25d9
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-03-06 17:09:25 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,19 @@ const app: Express = express();
 // connectToDataBase();
 
 configMiddlewares.forEach((middlewares) => app.use(middlewares));
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+// // File upload route
+// app.post("/upload", upload.array("files", 5), (req: Request, res: Response) => {
+//   if (!req.files) {
+//     return res.status(400).json({ message: "No files uploaded!" });
+//   }
+
+//   res.status(200).json({
+//     message: "Files uploaded successfully!",
+//     files: req.files,
+//   });
+// });
 app.use('/api', Routes);
 
 export default app;
