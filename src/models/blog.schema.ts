3bbb001dcffb8b6756be80f37be0fa2db181eaa4
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-03-10 18:38:35 +0530

    Add files via upload

---
+++ src/models/blog.schema.ts
@@ -0,0 +1,49 @@
+import mongoose, { Document, Schema } from "mongoose";
+
+interface IFAQ {
+  ques: string;
+  ans: string;
+}
+
+// Define the Blog interface
+export interface IBlog extends Document {
+  content: string;
+  metaTitle: string;
+  metaDescription: string;
+  metaKeyword: string;
+  pageUrl: string;
+  heading: string;
+  category: string;
+  status: boolean;
+  faqs: IFAQ[];
+  authorName: string;
+  authorDescription: string;
+  userid: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const BlogSchema: Schema<IBlog> = new Schema(
+  {
+    content: { type: String, required: true },
+    metaTitle: { type: String, required: true },
+    metaDescription: { type: String, required: true },
+    metaKeyword: { type: String, required: true },
+    pageUrl: { type: String, required: true, unique: true },
+    heading: { type: String, required: true },
+    category: { type: String, required: true },
+    status: { type: Boolean, default: true },
+    userid: { type: String, required: true },
+    faqs: [
+      {
+        ques: { type: String },
+        ans: { type: String },
+      },
+    ],
+    authorName: { type: String },
+    authorDescription: { type: String },
+  },
+  { timestamps: true }
+);
+
+export const Blogs = mongoose.model<IBlog>("Blog", BlogSchema);
