fcc070bf6461b464bb9a62d787d69edbf6183a3f
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-03-10 10:13:07 +0800

    upload image

--- src/app.ts
+++ src/app.ts
@@ -1,26 +1,33 @@
-import express, { Express } from 'express';
+import express, { Express, Request, Response } from 'express';
 // import connectToDataBase from './DBConnection';
 import configMiddlewares from './middlewares/config.middleware';
 import { Routes } from './routes/all.routes';
+import path from 'path';
+import upload from './middlewares/upload';
 
 const app: Express = express();
 
 // connectToDataBase();
 
 configMiddlewares.forEach((middlewares) => app.use(middlewares));
-// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// // File upload route
-// app.post("/upload", upload.array("files", 5), (req: Request, res: Response) => {
-//   if (!req.files) {
-//     return res.status(400).json({ message: "No files uploaded!" });
-//   }
+// File upload route
+app.post(
+  '/api/upload',
+  upload.array('files', 5),
+  (req: Request, res: Response): void => {
+    console.log(req.files);
+    if (!req.files) {
+      res.status(400).json({ message: 'No files uploaded!' });
+    }
 
-//   res.status(200).json({
-//     message: "Files uploaded successfully!",
-//     files: req.files,
-//   });
-// });
+    res.status(200).json({
+      message: 'Files uploaded successfully!',
+      files: req.files,
+    });
+  }
+);
 app.use('/api', Routes);
 
 export default app;
