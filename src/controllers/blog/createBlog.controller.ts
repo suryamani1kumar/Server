395d14ac5154584292b2b99e6a70051909c5e816
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-03-10 18:38:04 +0530

    Update createBlog.controller.ts

--- src/controllers/blog/createBlog.controller.ts
+++ src/controllers/blog/createBlog.controller.ts
@@ -1,7 +1,12 @@
-import { Request, Response } from 'express';
-import saveJsonToFile from '../../services/saveJson';
+import { Request, Response } from "express";
+import saveJsonToFile from "../../services/saveJson";
+import { Blogs } from "../../models/blog.schema";
 
-export const createBlog = (req: Request, res: Response) => {
-  saveJsonToFile('blogData.json', req.body);
-  res.send('Data add successfully');
+export const createBlog = async (req: Request, res: Response) => {
+  saveJsonToFile("blogData.json", req.body);
+  const createblog = await new Blogs(req.body);
+
+  await createblog.save();
+
+  res.status(200).json({ message: "blog Added", blog: createblog });
 };
