2f8babec19f87c76f74e66851e277f5494071034
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-03-10 11:39:30 +0530

    Update upload.ts

--- src/middlewares/upload.ts
+++ src/middlewares/upload.ts
@@ -1,19 +1,31 @@
 import multer, { FileFilterCallback } from "multer";
 import { Request } from "express";
 import path from "path";
+import fs from "fs";
+
+// create image dir
+const uploadDir = path.join(process.cwd(), "uploads");
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
 // Define file storage configuration
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Save files in 'uploads/' folder
+    cb(null, uploadDir); // Save files in 'uploads/' folder
   },
+
   filename: (req, file, cb) => {
-    cb(null, `${Date.now()}${path.extname(file.originalname)}`); // Unique filename
+    cb(null, `${Date.now()}_${file.originalname}`); // Unique filename
   },
 });
 
 // Define file filter
-const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallback) => {
+const fileFilter = (
+  req: Request,
+  file: Express.Multer.File,
+  cb: FileFilterCallback
+) => {
   const allowedTypes = ["image/jpeg", "image/png", "application/pdf"];
   if (allowedTypes.includes(file.mimetype)) {
     cb(null, true);
@@ -25,7 +37,7 @@ const fileFilter = (req: Request, file: Express.Multer.File, cb: FileFilterCallb
 // Configure Multer
 const upload = multer({
   storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 }, // 5MB limit
+  limits: { fileSize: 2 * 1024 * 1024 }, // 5MB limit
   fileFilter: fileFilter,
 });
 
