720acd4d8e8e0f3674bb8d5422ccffddb46b9979
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-03-10 11:41:10 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,17 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express} from "express";
 // import connectToDataBase from './DBConnection';
-import configMiddlewares from './middlewares/config.middleware';
-import { Routes } from './routes/all.routes';
-import path from 'path';
-import upload from './middlewares/upload';
+import configMiddlewares from "./middlewares/config.middleware";
+import { Routes } from "./routes/all.routes";
+import path from "path";
 
 const app: Express = express();
 
 // connectToDataBase();
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 configMiddlewares.forEach((middlewares) => app.use(middlewares));
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-// File upload route
-app.post(
-  '/api/upload',
-  upload.array('files', 5),
-  (req: Request, res: Response): void => {
-    console.log(req.files);
-    if (!req.files) {
-      res.status(400).json({ message: 'No files uploaded!' });
-    }
-
-    res.status(200).json({
-      message: 'Files uploaded successfully!',
-      files: req.files,
-    });
-  }
-);
-app.use('/api', Routes);
+app.use("/api", Routes);
 
 export default app;
+
