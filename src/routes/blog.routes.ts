574bdbc39cdf5e2449b3c4e778a3ede44b891010
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-03-20 17:24:07 +0530

    Update blog.routes.ts

--- src/routes/blog.routes.ts
+++ src/routes/blog.routes.ts
@@ -2,11 +2,15 @@ import { Router } from "express";
 import { createBlog } from "../controllers/blog/createBlog.controller";
 import { getBlog } from "../controllers/blog/blog.controller";
 import { updateBlog } from "../controllers/blog/updateBlog.controller";
+import { allBlog } from "../controllers/blog/allBlog.controller";
+import { searchBlog } from "../controllers/blog/searchBlog.controller";
 
 const router = Router();
 
 router.post("/addBlog", createBlog);
 router.get("/blog", getBlog);
+router.get("/allBlog", allBlog);
+router.get("/searchBlog", searchBlog);
 router.post("/updateBlog", updateBlog);
 
 export default router;
