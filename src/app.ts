7ee3bd6d2d2fa3ce36183fd1b7b5647bb77525d6
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-03-20 07:35:08 +0530

    change in blog

--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,16 @@
-import express, { Express} from "express";
+import express, { Express } from 'express';
 // import connectToDataBase from './DBConnection';
-import configMiddlewares from "./middlewares/config.middleware";
-import { Routes } from "./routes/all.routes";
-import path from "path";
+import configMiddlewares from './middlewares/config.middleware';
+import { Routes } from './routes/all.routes';
+import path from 'path';
 
 const app: Express = express();
 
 // connectToDataBase();
-app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
+app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 configMiddlewares.forEach((middlewares) => app.use(middlewares));
 
-app.use("/api", Routes);
+app.use('/api', Routes);
 
 export default app;
-
