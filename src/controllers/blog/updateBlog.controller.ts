07b64d90d8fc7ebc9ea6bc3165aceeceff71362b
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-03-21 11:31:17 +0530

    Update updateBlog.controller.ts

--- src/controllers/blog/updateBlog.controller.ts
+++ src/controllers/blog/updateBlog.controller.ts
@@ -2,15 +2,41 @@ import { Request, Response } from "express";
 import { Blogs } from "../../models/blog.schema";
 
 export const updateBlog = async (req: Request, res: Response) => {
-  const pageUrl = req.query.pageurl;
-  const RequestData = req.body;
-  const updatedBlog = await Blogs.findOneAndUpdate({ pageUrl }, RequestData, {
-    new: true,
-  });
-  
-  if (!updatedBlog) {
-    res.status(404).json({ message: "Blog not found" });
-    return;
+  try {
+    const pageUrl = req.query.pageurl;
+    const RequestData = req.body;
+    const updatedBlog = await Blogs.findOneAndUpdate({ pageUrl }, RequestData, {
+      new: true,
+    });
+
+    if (!updatedBlog) {
+      res.status(404).json({ message: "Blog not found" });
+      return;
+    }
+    res.status(200).json({ message: "blog updated", blog: updatedBlog });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
   }
-  res.status(200).json({ message: "blog updated", blog: RequestData });
+};
+
+export const blogStatus = async (req: Request, res: Response) => {
+  try {
+    const pageUrl = req.query.pageurl;
+    const Active = req.query.Active;
+    const updatedBlog = await Blogs.findOneAndUpdate(
+      { pageUrl },
+      { Active },
+      {
+        new: true,
+      }
+    );
+
+    if (!updatedBlog) {
+      res.status(404).json({ message: "Blog not found" });
+      return;
+    }
+    res.status(200).json({ message: "blog updated", blog: updatedBlog });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
 };
