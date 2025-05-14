16ac7d43f0e7ae8e86ac4ee79381ee9a23aeebbe
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-05-14 18:12:59 +0530

    Add files via upload

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,23 +1,20 @@
-import dotenv from 'dotenv';
-import path from 'path';
+import dotenv from "dotenv";
+import path from "path";
 
-const envFile = `.env.${process.env.NODE_ENV || 'development'}`;
+const envFile = `.env.${process.env.NODE_ENV}`;
 
 dotenv.config({ path: path.resolve(process.cwd(), envFile) });
 
-const parsedOrigin = process.env.ORIGIN
-  ? JSON.parse(process.env.ORIGIN)
-  : ['http://localhost:3000', 'http://localhost:5173'];
+const parsedOrigin = JSON.parse(process.env.ORIGIN as string);
 
 export const config = {
-  PORT: process.env.PORT || 5000,
-  API_HEADER_KEY: process.env.API_HEADER_KEY || 'your-valid-api-key',
+  PORT: process.env.PORT,
+  API_HEADER_KEY: process.env.API_HEADER_KEY,
   DATABASE: {
-    DB_NAME: process.env.DB_NAME || 'mongodb://localhost:27017',
+    DB_NAME: process.env.DB_NAME,
   },
-  ACCESS_TOKEN_SECRET:
-    process.env.ACCESS_TOKEN_SECRET || 'tripLoom_5000_development',
-  REFRESH_TOKEN_SECRET:
-    process.env.REFRESH_TOKEN_SECRET || 'Triploom#5000#development',
+  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
+  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
   ORIGIN: parsedOrigin,
+  SERVER_URL: process.env.SERVER_URL,
 };
--- src/routes/blog.routes.ts
+++ src/routes/blog.routes.ts
@@ -8,10 +8,11 @@ import {
 import { allBlog } from "../controllers/blog/allBlog.controller";
 import { searchBlog } from "../controllers/blog/searchBlog.controller";
 import { deleteBlog } from "../controllers/blog/deleteBlog.controller";
+import upload from "../middlewares/upload";
 
 const router = Router();
 
-router.post("/addBlog", createBlog);
+router.post("/addBlog", upload.array("files", 5), createBlog);
 router.get("/blog", getBlog);
 router.get("/allBlog", allBlog);
 router.get("/blogs/search", searchBlog);
