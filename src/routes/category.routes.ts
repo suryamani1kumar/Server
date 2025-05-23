fa8be833b86f6bcd4d52e0a0a8f8e7376628bc89
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-05-23 17:07:50 +0530

    Add files via upload

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import { AddCategory } from "../controllers/category/addCategory.controller";
 import { AddSubCategory } from "../controllers/category/addSubCategory.controller";
+import {
+  getCategory,
+  getCategoryById,
+} from "../controllers/category/getCategory.controller";
+import upload from "../middlewares/upload";
 
 const router = Router();
 
-router.post("/category", AddCategory);
+router.post("/category", upload.array("file", 1), AddCategory);
 router.post("/subcategory/:id", AddSubCategory);
+router.get("/getcategory", getCategory);
+router.get("/categoryById/:id", getCategoryById);
 
 export default router;
