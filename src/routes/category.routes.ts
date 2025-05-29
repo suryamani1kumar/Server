9cc9429a5b5b67d5308767ca48a35c8b1c1d6fb9
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-05-29 14:27:31 +0530

    Update category.routes.ts

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -2,7 +2,8 @@ import { Router } from "express";
 import { AddCategory } from "../controllers/category/addCategory.controller";
 import { AddSubCategory } from "../controllers/category/addSubCategory.controller";
 import upload from "../middlewares/upload";
-import { getCategory, getCategoryById, searchCategory } from "../controllers/category/getCategory.controller";
+import { getCategory, getCategoryById, searchCategory } from "../controllers/category/getcategory.controller";
+
 
 const router = Router();
 
