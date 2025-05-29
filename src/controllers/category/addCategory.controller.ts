19f56d0edabd929077c53686e812a221ee74031b
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2025-05-29 14:23:16 +0530

    Update addCategory.controller.ts

--- src/controllers/category/addCategory.controller.ts
+++ src/controllers/category/addCategory.controller.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from "express";
 import { category } from "../../models/category.schema";
+import { config } from "../../config/config";
 
 export const AddCategory = async (req: Request, res: Response) => {
   try {
-    const categoryData = new category(req.body);
+    const file = req.files;
+    const Images = (file as Array<Express.Multer.File>)?.map(
+      (image: Express.Multer.File) =>
+        `${config.SERVER_URL}/uploads/${image.filename}`
+    );
+    const categoryData = new category({ ...req.body, image: Images });
     const savedData = await categoryData.save();
     res.status(200).json({ message: "Create category", data: savedData });
   } catch (error) {
