bf70fba1108abce43b3e1cc0598d8a13383cedd0
suryamani1kumar <8437suryamanikumar@gmail.com>
2025-06-14 16:08:18 +0530

    Add new api

--- src/controllers/blog/updateBlog.controller.ts
+++ src/controllers/blog/updateBlog.controller.ts
@@ -1,21 +1,60 @@
-import { Request, Response } from 'express';
-import { Blogs } from '../../models/blog.schema';
+import { Request, Response } from "express";
+import { Blogs } from "../../models/blog.schema";
+import { config } from "../../config/config";
 
 export const updateBlog = async (req: Request, res: Response) => {
   try {
-    const pageUrl = req.query.pageurl;
-    const RequestData = req.body;
-    const updatedBlog = await Blogs.findOneAndUpdate({ pageUrl }, RequestData, {
-      new: true,
-    });
+    const UpdatepageUrl = req.query.pageurl;
+    const file = req.files;
+    const Images = (file as Array<Express.Multer.File>)?.map(
+      (image: Express.Multer.File) =>
+        `${config.SERVER_URL}/image/${image.filename}?w=200`
+    );
+
+    const {
+      content,
+      metaTitle,
+      metaDescription,
+      metaKeyword,
+      pageUrl,
+      heading,
+      category,
+      active,
+      faqs,
+      authorName,
+      authorDescription,
+      userid,
+    } = req.body;
+    const faq = JSON.parse(faqs);
+    const updatedBlog = await Blogs.findOneAndUpdate(
+      { pageUrl: UpdatepageUrl },
+      {
+        content,
+        metaTitle,
+        metaDescription,
+        metaKeyword,
+        pageUrl,
+        heading,
+        category,
+        active,
+        faqs: faq,
+        images: Images,
+        authorName,
+        authorDescription,
+        userid,
+      },
+      {
+        new: true,
+      }
+    );
 
     if (!updatedBlog) {
-      res.status(404).json({ message: 'Blog not found' });
+      res.status(404).json({ message: "Blog not found" });
       return;
     }
-    res.status(200).json({ message: 'blog updated', blog: updatedBlog });
+    res.status(200).json({ message: "blog updated", blog: updatedBlog });
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error', error });
+    res.status(500).json({ message: "Internal server error", error });
   }
 };
 
@@ -32,11 +71,11 @@ export const blogStatus = async (req: Request, res: Response) => {
     );
 
     if (!updatedBlog) {
-      res.status(404).json({ message: 'Blog not found' });
+      res.status(404).json({ message: "Blog not found" });
       return;
     }
-    res.status(200).json({ message: 'blog updated', blog: updatedBlog });
+    res.status(200).json({ message: "blog updated", blog: updatedBlog });
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error', error });
+    res.status(500).json({ message: "Internal server error", error });
   }
 };
--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -3,6 +3,7 @@ import { AddCategory } from "../controllers/category/addCategory.controller";
 import { AddSubCategory } from "../controllers/category/addSubCategory.controller";
 import upload from "../middlewares/upload";
 import { getCategory, getCategoryById, searchCategory } from "../controllers/category/getCategory.controller";
+import { ActiveCategory } from "../controllers/category/updateCategory.controller";
 
 
 const router = Router();
@@ -10,6 +11,7 @@ const router = Router();
 router.post("/category", upload.array("file", 1), AddCategory);
 router.post("/subcategory/:id", AddSubCategory);
 router.get("/getcategory", getCategory);
+router.get("/statusCategory/:id", ActiveCategory);
 router.get("/categoryById/:id", getCategoryById);
 router.get("/searchcategory", searchCategory);
 
