c3d3dafac748e20ce5ec9624da0b5ac9b5e143cd
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-01-14 12:24:57 +0530

    Update category.routes.ts

--- src/routes/category.routes.ts
+++ src/routes/category.routes.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 import { AddCategory } from "../controllers/category/addCategory.controller";
-import { AddSubCategory } from "../controllers/category/addSubCategory.controller";
 import upload from "../middlewares/upload";
 import { ActiveCategory } from "../controllers/category/updateCategory.controller";
 import { getCategory, getCategoryById, searchCategory } from "../controllers/category/getcategory.controller";
@@ -8,11 +7,11 @@ import { getCategory, getCategoryById, searchCategory } from "../controllers/cat
 const router = Router();
 
 router.post("/category", upload.array("file", 1), AddCategory);
-router.post("/subcategory/:id", AddSubCategory);
 router.get("/getcategory", getCategory);
 router.get("/statusCategory/:id", ActiveCategory);
 router.get("/categoryById/:id", getCategoryById);
 router.get("/searchcategory", searchCategory);
 
 export default router;
+
 
