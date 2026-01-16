cb35d3045d7c3626008ae9f1d45f3494d5d58cad
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-01-16 13:28:15 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express, { Express } from "express";
-import { Request, Response } from 'express';
 import connectToDataBase from "./DBConnection";
 import configMiddlewares from "./middlewares/config.middleware";
 import { protectedRoutes, publicRoutes } from "./routes/all.routes";
 import { getImage } from "./controllers/image/image.controller";
+import defaultUser from "./config/defaultUser";
 
 const app: Express = express();
 
-connectToDataBase();
+connectToDataBase().then(() => defaultUser());
 
 app.use("/image/:filename", getImage);
 
@@ -18,13 +18,5 @@ configMiddlewares.forEach((middlewares) => app.use(middlewares));
 
 // private Routes
 app.use("/api", protectedRoutes);
-app.use("/", async (req: Request, res: Response) => {
-  try {
-    res.status(200).send(`<h1>Hello World </h1>`)
-  } catch (error) {
-    res.status(500).json({ message: 'Internal server error', error });
-  }
-});
-
 
 export default app;
