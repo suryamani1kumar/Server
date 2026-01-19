8427d5af2cb936008d565b68f39e3b42c9aea3b2
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-01-19 22:43:11 +0530

    Update updateUser.controller.ts

--- src/controllers/user/updateUser.controller.ts
+++ src/controllers/user/updateUser.controller.ts
@@ -1,21 +1,39 @@
-import { Request, Response } from 'express';
-import { User } from '../../models/user.schema';
+import { Request, Response } from "express";
+import { User } from "../../models/user.schema";
 
 export const updateUser = async (req: Request, res: Response) => {
   try {
-    const userid = req.query.userid;
-    const RequestData = req.body;
-    const updatedUser = await User.findOneAndUpdate({ userid }, RequestData, {
-      new: true,
-    });
+    const { userid } = req.query;
+    const requestData = req.body;
 
-    if (!updatedUser) {
-      res.status(404).json({ message: 'user not found' });
+    if (!userid) {
+      res.status(400).json({ message: "User ID is required" });
       return;
     }
-    res.status(200).json({ message: 'user updated', user: updatedUser });
+
+    const updatedUser = await User.findByIdAndUpdate(
+      { _id: userid },
+      { $set: requestData },
+      {
+        new: true,
+        runValidators: true,
+      },
+    );
+
+    if (!updatedUser) {
+      res.status(404).json({ message: "User not found" });
+      return;
+    }
+
+    res.status(200).json({
+      message: "User updated successfully",
+      user: updatedUser,
+    });
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error', error });
+    console.error(error);
+    res.status(500).json({
+      message: "Internal server error",
+    });
   }
 };
 
@@ -24,19 +42,19 @@ export const userStatus = async (req: Request, res: Response) => {
     const userid = req.query.userid;
     const isActive = req.query.isActive;
     const statusUpdate = await User.findOneAndUpdate(
-      { userid },
+      { _id: userid },
       { isActive },
       {
         new: true,
-      }
+      },
     );
 
     if (!statusUpdate) {
-      res.status(404).json({ message: 'user not found' });
+      res.status(404).json({ message: "user not found" });
       return;
     }
-    res.status(200).json({ message: 'user updated', user: statusUpdate });
+    res.status(200).json({ message: "user updated", user: statusUpdate });
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error', error });
+    res.status(500).json({ message: "Internal server error", error });
   }
 };
