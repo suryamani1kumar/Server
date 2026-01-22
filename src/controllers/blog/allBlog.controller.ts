19f2e30a3c64f35349788fe506e02bc55ee50baa
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-01-22 11:10:13 +0530

    Update allBlog.controller.ts

--- src/controllers/blog/allBlog.controller.ts
+++ src/controllers/blog/allBlog.controller.ts
@@ -1,27 +1,23 @@
-import { Request, Response } from 'express';
-import { Blogs } from '../../models/blog.schema';
+import { Request, Response } from "express";
+import { Blogs } from "../../models/blog.schema";
 
 export const allBlog = async (req: Request, res: Response) => {
   try {
     const page: number = parseInt(req.query.page as string) || 1;
     const limit: number = parseInt(req.query.limit as string) || 4;
-    const blog = await Blogs.find(
-      // {},
-      // { pageUrl: 1, heading: 1, category: 1, active: 1 },
-      // {
-      //   new: true,
-      // }
-    )
+    const blog = await Blogs.find()
+      .select("pageUrl heading isActive")
+      .populate("category", "name")
       .skip((page - 1) * limit)
       .limit(limit)
       .lean();
     if (!blog) {
-      res.status(404).json({ message: 'Blog is not exist' });
+      res.status(404).json({ message: "Blog is not exist" });
       return;
     }
     const totalCount: number = await Blogs.countDocuments();
     res.status(200).json({
-      message: 'blog fetch All',
+      message: "blog fetch All",
       page,
       limit,
       totalPages: Math.ceil(totalCount / limit),
@@ -29,6 +25,6 @@ export const allBlog = async (req: Request, res: Response) => {
       blog: blog,
     });
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error', error });
+    res.status(500).json({ message: "Internal server error", error });
   }
 };
