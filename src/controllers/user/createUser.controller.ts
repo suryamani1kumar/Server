bf6e1fa820f3d34448de358009abacb2e6f682d5
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-01-22 11:05:46 +0530

    Update createUser.controller.ts

--- src/controllers/user/createUser.controller.ts
+++ src/controllers/user/createUser.controller.ts
@@ -1,7 +1,6 @@
 import { Request, Response } from "express";
-import { hashPassword } from "../../services/password";
+
 import { User } from "../../models/user.schema";
-import { v4 as uuid } from 'uuid';
 
 export const CreateUser = async (req: Request, res: Response) => {
   try {
@@ -15,17 +14,64 @@ export const CreateUser = async (req: Request, res: Response) => {
       res.status(302).json({ message: "User exists, please log in" });
       return;
     }
-    // password hash
-    const hashedPassword = await hashPassword(RequestData.password);
-    RequestData.password = hashedPassword;
-    const userid = uuid()
 
     // create user
-    const userData = await new User({ ...RequestData, userid });
+    const userData = await new User({ ...RequestData });
     await userData.save();
 
     res.status(200).json({ message: "User registered ", user: userData });
   } catch (error) {
-    res.status(500).json({ message: 'Internal server error', error });
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response) => {
+  try {
+    const id = req.query.id;
+    const blog = await User.findOneAndDelete({ _id: id });
+    if (!blog) {
+      res.status(404).json({ message: "User is not exist" });
+      return;
+    }
+    res.status(200).json({ message: "User is deleted" });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
+
+export const HandlePassword = async (req: Request, res: Response) => {
+  try {
+    const { userId, newPassword } = req.body;
+
+    if (!userId || !newPassword) {
+      res.status(400).json({
+        success: false,
+        message: "User ID and new password are required",
+      });
+      return;
+    }
+
+    const user = await User.findOne({ _id: userId });
+    if (!user) {
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+      });
+      return;
+    }
+
+    user.password = newPassword;
+    await user.save();
+
+    res.status(200).json({
+      success: true,
+      message: "User password updated successfully",
+    });
+  } catch (error) {
+    console.error("Admin change user password error:", error);
+    res.status(500).json({
+      success: false,
+      message: "Server error",
+    });
   }
 };
