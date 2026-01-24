31a3be808a2b1b7ab8fe08f02aea080f5446da5c
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-01-24 16:37:40 +0530

    Update allBlog.controller.ts

--- src/controllers/blog/allBlog.controller.ts
+++ src/controllers/blog/allBlog.controller.ts
@@ -28,3 +28,32 @@ export const allBlog = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Internal server error", error });
   }
 };
+
+export const allBlogWeb = async (req: Request, res: Response) => {
+  try {
+    const page: number = parseInt(req.query.page as string) || 1;
+    const limit: number = parseInt(req.query.limit as string) || 4;
+    const blog = await Blogs.find({ isActive: true })
+      .select("pageUrl heading smallDescription images")
+      .populate("category", "name")
+      .populate("author", "name")
+      .skip((page - 1) * limit)
+      .limit(limit)
+      .lean();
+    if (!blog) {
+      res.status(404).json({ message: "Blog is not exist" });
+      return;
+    }
+    const totalCount: number = await Blogs.countDocuments();
+    res.status(200).json({
+      message: "blog fetch All",
+      page,
+      limit,
+      totalPages: Math.ceil(totalCount / limit),
+      totalBlogs: totalCount,
+      blog: blog,
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
