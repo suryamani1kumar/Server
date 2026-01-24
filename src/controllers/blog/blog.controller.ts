bf34045d4f86a253dd12343f29463f99a241d75b
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-01-24 16:36:56 +0530

    Update blog.controller.ts

--- src/controllers/blog/blog.controller.ts
+++ src/controllers/blog/blog.controller.ts
@@ -14,3 +14,23 @@ export const getBlog = async (req: Request, res: Response) => {
     res.status(500).json({ message: "Internal server error", error });
   }
 };
+
+export const blogBySlug = async (req: Request, res: Response) => {
+  try {
+    const pageurl = req.query.pageurl;
+    const blog = await Blogs.findOne({
+      pageUrl: pageurl,
+      isActive: true,
+    })
+      .populate("category", "name slug description image")
+      .populate("author", "name jobtitle description image");
+
+    if (!blog) {
+      res.status(404).json({ message: "Blog is not exist" });
+      return;
+    }
+    res.status(200).json({ message: "blog fetch", blog: blog });
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error", error });
+  }
+};
