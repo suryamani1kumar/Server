1fc4ab75fe2d9e9c8cc3235ad79801591c34b576
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-02-12 12:43:40 +0530

    Update uploadFile.routes.ts

--- src/routes/uploadFile.routes.ts
+++ src/routes/uploadFile.routes.ts
@@ -1,10 +1,17 @@
 import { Router } from "express";
-import { deleteUploadFile, uploadFile } from "../controllers/uploadfile/uploadfile.controller";
+import {
+  AddFile,
+  deleteUploadFile,
+  GetAllFile,
+  uploadFile,
+} from "../controllers/uploadfile/uploadfile.controller";
 import { upload } from "../middlewares/upload";
 
 const router = Router();
 
 router.post("/upload", upload.single("file"), uploadFile);
 router.delete("/fileDelete", deleteUploadFile);
+router.post("/saveFile", AddFile);
+router.get("/getFile", GetAllFile);
 
 export default router;
