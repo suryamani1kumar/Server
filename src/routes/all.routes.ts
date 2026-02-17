2ec3bc2c6528c176d1610cc25cf10ff0517f9a44
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-02-17 13:33:28 +0530

    Update all.routes.ts

--- src/routes/all.routes.ts
+++ src/routes/all.routes.ts
@@ -1,7 +1,6 @@
 import userRoutes from "./user.routes";
 import blogRoutes from "./blog.routes";
 import categoryRoutes from "./category.routes";
-import trackingRoutes from "./trackingpixel.routes";
 import Author from "./author.routes";
 import Sitemap from "./sitemap.routes";
 import uploadImage from "./uploadFile.routes";
@@ -10,7 +9,6 @@ export const protectedRoutes = [
   userRoutes,
   categoryRoutes,
   blogRoutes,
-  trackingRoutes,
   Author,
   Sitemap,
   uploadImage,
