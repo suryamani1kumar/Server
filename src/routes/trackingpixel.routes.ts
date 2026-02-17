ad102f9ac52826b54f10393f25cd7700cca1c0c1
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-02-17 13:32:18 +0530

    Delete src/routes/trackingpixel.routes.ts

--- src/routes/trackingpixel.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { trackingpixel } from "../controllers/tracking/trackingpixel.controller";
-
-const router = Router();
-
-router.post("/track", trackingpixel);
-
-export default router;
