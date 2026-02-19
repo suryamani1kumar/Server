af48b12dd39a439e897793b6a5be0291241f9b1f
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-02-19 13:29:06 +0530

    Update deslocation.routes.ts

--- src/routes/deslocation.routes.ts
+++ src/routes/deslocation.routes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import {
   createLocation,
+  getCity,
+  getCountry,
   getLocation,
 } from "../controllers/destination/location.controller";
 import {
   createDestination,
+  deleteDestination,
   getAllDestinations,
+  getDestinationBySlug,
   updateDestination,
 } from "../controllers/destination/destination.controller";
 
@@ -13,8 +17,12 @@ const router = Router();
 
 router.post("/addlocation", createLocation);
 router.get("/getlocation", getLocation);
+router.get("/getCountry", getCountry);
+router.get("/getCity", getCity);
 router.post("/adddestination", createDestination);
 router.put("/updatedestination", updateDestination);
 router.get("/getdestination", getAllDestinations);
+router.get("/slugdestination", getDestinationBySlug);
+router.delete("/deleteDes", deleteDestination);
 
 export default router;
