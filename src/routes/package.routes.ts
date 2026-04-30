0ed42908acff44510bb93619f3606222d30b83d0
Suryamani kumar <105836069+suryamani1kumar@users.noreply.github.com>
2026-04-30 19:29:13 +0530

    Update package.routes.ts

--- src/routes/package.routes.ts
+++ src/routes/package.routes.ts
@@ -7,15 +7,18 @@ import {
   getPackageBySlugWeb,
   updatePackage,
 } from "../controllers/package/packagecontroller";
+import { createPackageCatgory, getPackageCatgory, updatePackageCategory } from "../controllers/package/pkgcategorycontroller";
 
 const router = Router();
-
-router.post("/createPackage", createPackage);
-router.put("/updatePackage", updatePackage);
-router.get("/getAllPacakge", getAllPackage);
-router.get("/getPackageBySlugWeb", getPackageBySlugWeb);
-router.get("/getPackageBySlug", getPackageBySlug);
-router.delete("/deletePackageBySlug", deletePackageBySlug);
-// router.patch("/activePackageBySlug", isActivePackageBySlug);
+router.post("/package/createPackageCatgory", createPackageCatgory);
+router.put("/package/updatePkgCategory", updatePackageCategory);
+router.get("/package/packageCatgory", getPackageCatgory);
+router.post("/package/createPackage", createPackage);
+router.put("/package/updatePackage", updatePackage);
+router.get("/package/getAllPacakge", getAllPackage);
+router.get("/package/getPackageBySlugWeb", getPackageBySlugWeb);
+router.get("/package/getPackageBySlug", getPackageBySlug);
+router.delete("/package/deletePackageBySlug", deletePackageBySlug);
+// router.patch("/package/activePackageBySlug", isActivePackageBySlug);
 
 export default router;
